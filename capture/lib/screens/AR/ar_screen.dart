d3c7848ff0ad9ac98ec2d3a4ec239c77eb71ed5a
joon0447 <joon0447@naver.com>
2025-05-30 00:42:59 +0900

    ar

---
+++ capture/lib/screens/AR/ar_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class ARScreen extends StatefulWidget {
+  const ARScreen({super.key});
+
+  @override
+  State<ARScreen> createState() => _ARScreenState();
+}
+
+class _ARScreenState extends State<ARScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
